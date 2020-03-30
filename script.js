abec2e500bbb8e7daf631b390ea34f831ef5edea
ics-nishihara <nishihara@ics-web.jp>
2020-03-30 16:01:16 +0900

    不要な記述削除、真偽判定を変更

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const validate = () => {
-  const invalidForm = document.querySelector("form:invalid");
+  const validForm = document.querySelector("form:valid");
   const submitButton = document.querySelector("#submit");
-  submitButton.disabled = invalidForm != null;
-  console.log(invalidForm)
-  // console.log(document.querySelector("form").validity.valid)
+  submitButton.disabled = validForm == null;
 };
 
 validate();
