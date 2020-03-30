1540f376f230fca805249a3a302388f57971bd8c
ics-nishihara <nishihara@ics-web.jp>
2020-03-30 15:50:53 +0900

    不適切なCSSの修正、スクリプトの整理、送信ボタンのラベル変更

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
+const validate = () => {
+  const invalidForm = document.querySelector("form:invalid");
+  const submitButton = document.querySelector("#submit");
+  submitButton.disabled = invalidForm != null;
+  console.log(invalidForm)
+  // console.log(document.querySelector("form").validity.valid)
+};
+
 validate();
 
 document.querySelectorAll("input,textarea").forEach(input => {
   input.addEventListener("input", validate);
 });
-
-function buttonValidate() {
-  const invalidForm = document.querySelector("form:invalid");
-  const submitButton = document.querySelector("#submit");
-  submitButton.disabled = invalidForm != null;
-}
-
-
-function validate() {
-  buttonValidate();
-}
