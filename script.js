04f87ac5c18e797edf1e6d50f0bd83ebe6249d4c
ics-nishihara <nishihara@ics-web.jp>
2020-04-03 19:26:15 +0900

    jsにコメント追加

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+/**
+ * フォーム全体の妥当性をチェックします
+ */
 const validate = () => {
   const validForm = document.querySelector("form:valid");
   const submitButton = document.querySelector("#submit");
   submitButton.disabled = validForm == null;
 };
 
+// 初期読み込み時に実行
 validate();
 
+// フォームに入力されたら、validate関数を実行
 document.querySelectorAll("input,textarea").forEach(input => {
   input.addEventListener("input", validate);
 });
